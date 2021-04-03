59ba4ebee8de456010138682038d8326838c7167
Shahinur01 <shahinurislam903@gmail.com>
2021-04-03 16:49:27 +0600

    first commit

--- index.js
+++ index.js
@@ -1,36 +1,69 @@
-const express = require('express')
-const MongoClient = require('mongodb').MongoClient;
-const cors = require('cors');
-require('dotenv').config();
 
+const express = require("express");
 const app = express();
-app.use(express.json());
+const ObjectID = require("mongodb").ObjectID;
+require("dotenv").config();
+const cors = require("cors");
 app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 5000;
+const MongoClient = require("mongodb").MongoClient;
 
-const port = process.env.PORT || 5000
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qwb0j.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+
+client.connect((err) => {
+  console.log("success", err);
+  const productCollection = client.db(`${process.env.DB_NAME}`).collection("product");
+  const productOrderCollection = client.db(`${process.env.DB_NAME}`).collection("orderInfo");
+
+  app.post("/addProduct", (req, res) => {
+    const product = req.body;
+    console.log(product);
+    productCollection.insertOne(product).then((result) => {
+      console.log("Product Image", result.insertedCount);
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/showbook", (req, res) => {
+    productCollection.find().toArray((err, items) => {
+      res.send(items);
+    });
+  });
+
+
+app.post('/addOrder', (req, res) => {
+  const orderDetails = req.body;
+    console.log(orderDetails)
+  productOrderCollection.insertOne(orderDetails)
+  .then(result => {
+    res.send(result.insertedCount > 0)
+  })
 })
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@database.1n8y8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    const eventCollection = client.db(`${process.env.DB_NAME}`).collection("foods");
-    console.log("Connected to database");
-
-    app.get('/events', (req, res) => {
-        eventCollection.find({})
-            .toArray((err, events) => {
-                res.send(events);
+   app.get('/orders', (req, res) => {
+        const qEmail = req.query.email;
+        productOrderCollection.find({ email: qEmail })
+            .toArray((err, documents) => {
+                res.send(documents)
+                
             })
     })
 
-    app.post('/addEvent', (req, res) => {
-        eventCollection.insertOne(req.body)
-            .then(result => res.send(!!result.insertedCount))
-    })
+    app.delete('/delete/:id', (req, res) => {
+        const id = ObjectID(req.params.id);
+        productCollection.deleteOne({_id: id})
+        .then(result => {
+          // res.send(result);
+          console.log(result);
+        })
+      })
+
+
 });
 
-app.listen(port)
+app.listen(port);
+
+
